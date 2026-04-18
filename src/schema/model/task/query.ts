98a33c880306371dd8332777edb61f77f310d073
Alejandro Castro <alejo199821@gmail.com>
2026-04-18 16:16:47 -0400

    complete error handling for queries and mutations

--- src/schema/model/task/query.ts
+++ src/schema/model/task/query.ts
@@ -1,5 +1,6 @@
 import builder from "../../builder";
 import { TaskType } from "./type";
+import { notFound } from "../../../errors";
 import { z } from "zod";
 
 builder.queryType({
@@ -10,12 +11,16 @@ builder.queryType({
       nullable: true,
       args: { id: t.arg.id({ required: true }) },
       validate: taskQuerySchema,
-      resolve: (_, { id }, ctx) =>
-        ctx.prisma.task.findUnique({ where: { id } }),
+      resolve: async (_, { id }, ctx) => {
+        const task = await ctx.prisma.task.findUnique({ where: { id } });
+        if (!task) notFound("Task", id);
+        return task;
+      },
     }),
     tasks: t.field({
       type: [TaskType],
       nullable: false,
+      // No error handling needed here since we're just returning an empty array if there are no tasks, which is a valid response.
       resolve: (_, {}, ctx) => ctx.prisma.task.findMany({}),
     }),
   }),
