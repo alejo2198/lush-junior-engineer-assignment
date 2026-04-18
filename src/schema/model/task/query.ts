0be356ac5d898afa670412ab2b1b5efc268dc41a
Alejandro Castro <alejo199821@gmail.com>
2026-04-18 13:21:57 -0400

    complete queries

---
+++ src/schema/model/task/query.ts
@@ -0,0 +1,27 @@
+import builder from "../../builder";
+import { TaskType } from "./type";
+import { z } from "zod";
+
+builder.queryType({
+  fields: (t) => ({
+    //Query to get a single task by ID and to get all tasks
+    task: t.field({
+      type: TaskType,
+      nullable: true,
+      args: { id: t.arg.id({ required: true }) },
+      validate: taskQuerySchema,
+      resolve: (_, { id }, ctx) =>
+        ctx.prisma.task.findUnique({ where: { id } }),
+    }),
+    tasks: t.field({
+      type: [TaskType],
+      nullable: false,
+      resolve: (_, {}, ctx) => ctx.prisma.task.findMany({}),
+    }),
+  }),
+});
+
+// ZOD SCHEMAS
+const taskQuerySchema = z.object({
+  id: z.uuid(),
+});
